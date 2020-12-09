199dcfc145f7fc8569377de1ba4e507bca2dfc33
Michael Neuenfeld <michael.neuenfeld@gmail.com>
2020-12-09 21:31:59 +0100

    solved day09

--- src/day09/p1.rs
+++ src/day09/p1.rs
@@ -1,81 +1,40 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::collections::HashMap;
-
-#[derive(Debug)]
-pub struct Loc {
-    inst: String,
-    val: i32,
-}
 
 pub fn doit() {
-    let reader = BufReader::new(File::open("data/input_day08.txt").unwrap());
-    let input: Vec<String> = reader.lines().map(|l| l.unwrap()).collect();
-    let data = prepare_data(&input);
-    let res = calc(&data);
+    let reader = BufReader::new(File::open("data/input_day09.txt").unwrap());
+    let input: Vec<usize> = reader.lines().map(|l| l.unwrap().parse::<usize>().unwrap()).collect();
+    let res = calc(&input, 25);
     println!("Result of day09 p1: {}", res);
 }
 
-fn prepare_data(data:&Vec<String>) -> Vec<Loc> {
-    data.iter().map(|l| { 
-        let ll:Vec<String> = l.split(" ").map(|x| String::from(x)).collect();
-        Loc { inst: String::from(&ll[0]), val: ll[1].parse::<i32>().unwrap() }
-    }).collect()
-}
-
-fn calc(data: &Vec<Loc>) -> i32 {
-    let mut vis:HashMap<usize, usize> = HashMap::new();
-    let mut acc:i32 = 0;
-    let mut p:usize = 0;
-    let limit:usize = 1; 
-    loop {
-        match vis.get_mut(&p) {
-            Some(x) => { 
-                if *x >= limit {
+fn calc(data: &Vec<usize>, preamble:usize) -> usize{
+    let mut res:Option<usize> = None;
+    for (i,d) in data[preamble+1..].iter().enumerate() {
+        let mut ok:bool = false;
+        for (j,v1) in data[i..=preamble+i-1].iter().enumerate() {
+            for v2 in data[i+1+j..=preamble+i].iter() {
+                if v1 + v2 == *d {
+                    ok = true;
                     break;
                 }
-                else {
-                    *x += 1;
+                if ok {
+                    break;
                 }
-            },
-            None => { vis.insert(p, 1); },
+            } 
         }
-        match &data[p] {
-            Loc { inst, val } if inst == "acc" => { 
-                acc += val;
-                p += 1;
-            },
-            Loc { inst, val } if inst == "jmp" => {
-                if p as i32 + *val < 0 {
-                    println!("Unrechable address");
-                } else {
-                    p = (p as i32 + *val) as usize;
-                }
-            },
-            Loc { inst, val:_ } if inst == "nop" => {
-                p += 1;
-            },
-            &_ => { println!("Unknown instruction")}
+        if !ok {
+            res = Some(*d);
+            break;
         }
     }
-    acc
+    res.unwrap()
 }
 
 mod tests {
     #[test]
     fn test_calc() {
-        let input:Vec<String> = vec![
-            String::from("nop +0"),
-            String::from("acc +1"),
-            String::from("jmp +4"),
-            String::from("acc +3"),
-            String::from("jmp -3"),
-            String::from("acc -99"),
-            String::from("acc +1"),
-            String::from("jmp -4"),
-            String::from("acc +6"),
-        ];
-        let data = super::prepare_data(&input);
-        assert_eq!(super::calc(&data), 5);
+        let input:Vec<usize> = vec![35,20,15,25,47,40,62,55,65,95,102,117,150,182,127,219,299,277,309,576];
+        assert_eq!(super::calc(&input, 5), 127);
     }
 }
--- src/day09/p2.rs
+++ src/day09/p2.rs
@@ -1,108 +1,59 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::collections::HashMap;
-use std::mem;
-
-#[derive(Debug)]
-#[derive(Clone)]
-pub struct Loc {
-    inst: String,
-    val: i32,
-}
 
 pub fn doit() {
-    let reader = BufReader::new(File::open("data/input_day08.txt").unwrap());
-    let input: Vec<String> = reader.lines().map(|l| l.unwrap()).collect();
-    let data = prepare_data(&input);
-    let res = pcalc(&data);
+    let reader = BufReader::new(File::open("data/input_day09.txt").unwrap());
+    let input: Vec<usize> = reader.lines().map(|l| l.unwrap().parse::<usize>().unwrap()).collect();
+    let sres = scalc(&input, 25);
+    let res = calc(&input, sres);
     println!("Result of day09 p2: {}", res);
 }
 
-fn prepare_data(data:&Vec<String>) -> Vec<Loc> {
-    data.iter().map(|l| { 
-        let ll:Vec<String> = l.split(" ").map(|x| String::from(x)).collect();
-        Loc { inst: String::from(&ll[0]), val: ll[1].parse::<i32>().unwrap() }
-    }).collect()
+fn scalc(data: &Vec<usize>, preamble:usize) -> usize{
+    let mut res:Option<usize> = None;
+    for (i,d) in data[preamble+1..].iter().enumerate() {
+        let mut ok:bool = false;
+        for (j,v1) in data[i..=preamble+i-1].iter().enumerate() {
+            for v2 in data[i+1+j..=preamble+i].iter() {
+                if v1 + v2 == *d {
+                    ok = true;
+                    break;
+                }
+                if ok {
+                    break;
+                }
+            } 
+        }
+        if !ok {
+            res = Some(*d);
+            break;
+        }
+    }
+    res.unwrap()
 }
 
-fn pcalc(data: &Vec<Loc>) -> i32 {
-    let mut res = calc(&data);
-    let mut re:usize = 0;
-    while res.is_none() {
-        let mut mydata = data.clone();
-        loop {
-            let v = mydata[re].val;
-            if mydata[re].inst == "jmp" {
-                let _old = mem::replace(&mut mydata[re], Loc { inst:String::from("nop"), val: v });
-                re += 1;
+fn calc(data: &Vec<usize>, sres:usize) -> usize{
+    let mut res:Option<usize> = None;
+    for i in 0..data.len()-1 {
+        for j in 1..data.len()-i {
+            let e = &data[i..i+j];
+            if e.iter().sum::<usize>() == sres {
+                res = Some(e.iter().min().unwrap() + e.iter().max().unwrap());
                 break;
-            } else if mydata[re].inst == "nop" {
-                let _old = mem::replace(&mut mydata[re], Loc { inst:String::from("jmp"), val: v });
-                re += 1;
-                break;                
             }
-            re += 1;
         }
-        res = calc(&mydata);
-    }
-    res.unwrap()    
-}
-
-fn calc(data: &Vec<Loc>) -> Option<i32> {
-    let mut vis:HashMap<usize, usize> = HashMap::new();
-    let mut acc:i32 = 0;
-    let mut p:usize = 0;
-    let limit:usize = 1; 
-    loop {
-        if p == data.len() {
-            return Some(acc);
-        }
-        match vis.get_mut(&p) {
-            Some(x) => { 
-                if *x >= limit {
-                    return None;
-                }
-                else {
-                    *x += 1;
-                }
-            },
-            None => { vis.insert(p, 1); },
-        }
-        match &data[p] {
-            Loc { inst, val } if inst == "acc" => { 
-                acc += val;
-                p += 1;
-            },
-            Loc { inst, val } if inst == "jmp" => {
-                if p as i32 + *val < 0 {
-                    println!("Unreachable address");
-                } else {
-                    p = (p as i32 + *val) as usize;
-                }
-            },
-            Loc { inst, val:_ } if inst == "nop" => {
-                p += 1;
-            },
-            &_ => { println!("Unknown instruction")}
+        if res.is_some() {
+            break;
         }
     }
+    res.unwrap()
 }
 
 mod tests {
     #[test]
     fn test_calc() {
-        let input:Vec<String> = vec![
-            String::from("nop +0"),
-            String::from("acc +1"),
-            String::from("jmp +4"),
-            String::from("acc +3"),
-            String::from("jmp -3"),
-            String::from("acc -99"),
-            String::from("acc +1"),
-            String::from("jmp -4"),
-            String::from("acc +6"),
-        ];
-        let data = super::prepare_data(&input);
-        assert_eq!(super::pcalc(&data), 8);
+        let input:Vec<usize> = vec![35,20,15,25,47,40,62,55,65,95,102,117,150,182,127,219,299,277,309,576];
+        let sres = super::scalc(&input, 5);
+        assert_eq!(super::calc(&input, sres), 62);
     }
 }
